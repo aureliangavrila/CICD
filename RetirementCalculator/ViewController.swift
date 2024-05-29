b6ff4c7e65082a7987c1fd86b56eda1bb00b6f83
Aurelian Gavrila <Aurelian.Gavrila@aerlingus.com>
2024-05-29 15:03:22 +0200

    Track events

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Analytics.trackEvent("navigated_to_calculator")
+    }
+
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+//        Crashes.generateTestCrash()
+        
+        Analytics.trackEvent("reirement_calculator_actioned")
     }
 
 
